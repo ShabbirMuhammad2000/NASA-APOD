44045b9e372a9cb88cf0c4f478d651ff90d5d138
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-08-07 21:23:38 -0400

    Fetched the Nasa images API

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// NASA API
+const count = 10
+const apiKey = 'DEMO_KEY'
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`
+
+let resultsArray = []
+
+// GET 10 Images from NASA API
+async function getNasaPictures() {
+  try {
+  const response = await fetch(apiUrl)
+  resultsArray = await response.json()
+  console.log(resultsArray)
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+
+// On load
+getNasaPictures()

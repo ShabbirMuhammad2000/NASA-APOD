113c62d0cf549cb3517d4842773de3e3fbbfa756
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-08-09 20:30:27 -0400

    Favorites are now stored in the DOM

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = []
 let favorites = {}
 
-function updateDOM() {
-  resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+  const currentArray = page === 'results' ? resultsArray : Object.values(favorites)
+  console.log('Current Array', page, currentArray)
+  currentArray.forEach((result) => {
     // Card Container
     const card = document.createElement('div')
     card.classList.add('card')
@@ -62,13 +64,21 @@ function updateDOM() {
   })
 }
 
+function updateDOM(page) {
+  // Get Favorites from LocalStorage
+  if(localStorage.getItem('nasaFavorites')) {
+    favorites = JSON.parse(localStorage.getItem('nasaFavorites'))
+    console.log('favorites from loacal storage', favorites)
+  }
+  createDOMNodes(page)
+}
+
 // GET 10 Images from NASA API
 async function getNasaPictures() {
   try {
   const response = await fetch(apiUrl)
   resultsArray = await response.json()
-  console.log(resultsArray)
-  updateDOM()
+  updateDOM('favorites')
   } catch (error) {
     // Catch Error Here
   }
